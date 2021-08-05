be44f3fffaef2cf5f9b8f3a252ead9eaeb67c642
Aeron Barajas <aeronbarajas@Aerons-MacBook-Air.local>
2021-08-05 12:36:11 -0500

    header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
 
     return  <div className="nav">
     <div className="nav-item"><span className="nav-logo">
-        <Link to="/">IGN</Link></span></div>
-    <div className="nav-item">
+        <Link to="/"><img className='ignlogo' src='https://images.squarespace-cdn.com/content/v1/5a4e740c4c326d723144bf21/1515109896684-6ZGJ7BT44OR0R0VOYJ1O/ign-logo-2color%402x.png'/></Link></span></div>
+    <div className="nav-item" className='articlesreroute'>
         <Link to="/articles">Articles</Link></div>
 
 </div>
